b65e203d03fc881be393b81cff63dee48dbe0c28
bingus <bendawsdev@gmail.com>
2025-11-05 13:48:23 -0600

    add the mod checker

--- Main.cs
+++ Main.cs
@@ -1,13 +1,19 @@
 using BepInEx;
 using BingusNametags.Plugins;
+
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
 using TMPro;
 using UnityEngine;
-using System.Text;
 
 [BepInDependency("bingus.nametags", DependencyFlags.HardDependency)]
 [BepInPlugin("bingus.grungus", "BingusBingusNametags", "1.0.0")]
 public class NametagLoader : BaseUnityPlugin
 {
+    public static Dictionary<string, string> mods = new Dictionary<string, string>();
+
     static void UpdateNametag(TextMeshPro tmpro, VRRig playerRig)
     {
         StringBuilder s = new StringBuilder();
@@ -22,30 +28,49 @@ public class NametagLoader : BaseUnityPlugin
         s.Append($"<color=green>{g}</color>");
         s.Append($"<color=blue>{b}</color>");
 
-        s.Append(" <color=gray>");
-
-        // Special cosmetics
-        string cosmetics = playerRig.concatStringOfCosmeticsAllowed;
-
-        if (playerRig.GetPlayerRef() == NetworkSystem.Instance.MasterClient)
-            s.Append("[MSR] ");
-        if (rig.concatStringOfCosmeticsAllowed.Contains("LBADE."))
-            s.Append("<color=red>[FG</color><color=blue>P]</color> ");
-        if (rig.concatStringOfCosmeticsAllowed.Contains("LBAGS."))
-            s.Append("<color=orange>[ILS]</color> ");
-        if (rig.concatStringOfCosmeticsAllowed.Contains("LBAAD."))
-            s.Append("<color=white>[ADM]</color> ");
-        if (rig.concatStringOfCosmeticsAllowed.Contains("LBAAK."))
-            s.Append("<color=yellow>[DEV]</color> ");
-        if (rig.Creator.GetPlayerRef().CustomProperties.Count > 1)
-            s.Append("[MDR] ");
-        
-        s.Append("</color>");
+        // PID
+        s.Append($"<color=red>{playerRig.OwningNetPlayer.UserId}</color>");
 
         tmpro.text = s.ToString();
     }
+
+    static void UpdateModsNametag(TextMeshPro tmpro, VRRig rig)
+    {
+        StringBuilder nametag = new StringBuilder();
+
+        foreach (string prop in rig.OwningNetPlayer.GetPlayerRef().CustomProperties.Keys)
+            if (mods.TryGetValue(prop.ToLower(), out string realModName))
+                nametag.Append($"[{realModName}]")
+
+        tmpro.text = nametag.ToString();
+    }
     
-    void Start() {
-        PluginManager.AddPluginUpdate(UpdateNametag, 1.2f, true);
+    void Start()
+    {
+        PluginManager.AddPluginUpdate(UpdateNametag, 1.2f, true);      // color code, pid
+        PluginManager.AddPluginUpdate(UpdateModsNametag, 0.6f, true);  // prop checker
+        
+        GorillaTagger.OnPlayerSpawned += OnGorillaInit;
+    }
+
+    private const string GorillaInfoURL = "https://raw.githubusercontent.com/HanSolo1000Falcon/GorillaInfo/main/";
+
+    void OnGorillaInit()
+    {
+        using (HttpClient httpClient = new HttpClient())
+        {
+            HttpResponseMessage knownModsResponse = httpClient.GetAsync(GorillaInfoURL + "KnownMods.txt").Result;
+            
+            knownModsResponse.EnsureSuccessStatusCode();
+
+            using (Stream stream = knownModsResponse.Content.ReadAsStreamAsync().Result)
+            using (StreamReader reader = new StreamReader(stream))
+                mods = JsonConvert.DeserializeObject<Dictionary<string, string>>(reader.ReadToEnd());
+        }
+
+        foreach (string prop in mods.Keys) {
+            mods[prop.ToLower()] = mods[prop];
+            mods.Remove(prop); // remove OG thing
+        }
     }
 }
